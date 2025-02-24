3bbddee0c17a52ad68d3c12c2bfeddae32b48144
Manvi <41837863+Manvipandya@users.noreply.github.com>
2025-02-24 03:47:23 -0500

    Update run.py
    with time

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+import datetime
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
-
-app.run(host='0.0.0.0',
-        port=8080,
-        debug=True)
+@app.route('/time')
+def get_time():
+    # Get the current day of the week
+    return str(datetime.datetime.now().strftime('%Y %H:%M:%S'))
+    
+app.run(host='0.0.0.0', port=8080, debug=True)
